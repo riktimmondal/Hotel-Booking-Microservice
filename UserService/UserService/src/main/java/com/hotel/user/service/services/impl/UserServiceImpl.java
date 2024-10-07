fcb1c3a4f40f1aac2233f601934ba664d8674b1e
riktimmondal <riktimrules@gmail.com>
2024-10-07 10:23:12 +0530

    Add RestTemplate to call rating service from user service

--- UserService/UserService/src/main/java/com/hotel/user/service/services/impl/UserServiceImpl.java
+++ UserService/UserService/src/main/java/com/hotel/user/service/services/impl/UserServiceImpl.java
@@ -1,11 +1,17 @@
 package com.hotel.user.service.services.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.hotel.user.service.entities.Rating;
 import com.hotel.user.service.entities.User;
 import com.hotel.user.service.exceptions.ResourceNotFoundException;
 import com.hotel.user.service.repositories.UserRepository;
@@ -16,6 +22,11 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User saveUser(User user) {
@@ -31,7 +42,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server for :"+userId));
+        
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server for :"+userId));
+        
+        ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
+        
+        logger.info("{}",ratingsOfUser);
+
+        user.setRatings(ratingsOfUser);
+
+        return user;
     }
     
 }
